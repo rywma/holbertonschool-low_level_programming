61eb239d2a20f2e5235987fdf8dec6b9bc1f1908
rywma <almujallireema@gmail.com>
2026-03-04 21:27:01 +0000

    Add task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = 1; col <= (size - row); col++)
+				_putchar(' ');
+			for (col = 1; col <= row; col++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
