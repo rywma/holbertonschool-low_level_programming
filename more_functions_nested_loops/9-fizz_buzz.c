e0c46427c30ace944a264594483074ac9be27f53
rywma <almujallireema@gmail.com>
2026-03-04 21:24:59 +0000

    Add task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints numbers 1 to 100, replacing multiples of 3 with Fizz,
+ * multiples of 5 with Buzz, and multiples of both with FizzBuzz.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int i;
