5f1b7c6e218678d96e4ce54df97faf8773d85865
rywma <almujallireema@gmail.com>
2026-03-05 18:51:32 +0000

    Add task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: the string to print
+ *
+ * Return: void
+ */
 void _puts(char *str)
 {
 	while (*str != '\0')
