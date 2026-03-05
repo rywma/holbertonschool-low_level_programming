1808f246bc7bfdec68daf4c29450f4625ab12145
rywma <almujallireema@gmail.com>
2026-03-05 19:12:26 +0000

    Add task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: integer value
+ */
+int _atoi(char *s)
+{
+	int i, sign;
+	unsigned int res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		res = res * 10 + (s[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
