824c82aca6815aaa3ccbae787232cfd9bcb93264
rywma <almujallireema@gmail.com>
2026-03-07 18:13:27 +0000

    Add task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate strings using n bytes from src
+ * @dest:destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return:pointer to resulting string destination
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src [j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
