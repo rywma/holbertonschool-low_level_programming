c6f7a760d807578bb74365dfd9ebdedca73305a8
rywma <almujallireema@gmail.com>
2026-03-09 19:56:28 +0000

    Add task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - fill memory
+ * @s: memory area filled
+ * @b: char to copy
+ * @n: number of lines to copy b
+ *
+ * Return: pointer to memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
