b8626cd8aa2314ee05417474952364dfede66a9e
rywma <almujallireema@gmail.com>
2026-03-09 20:12:45 +0000

    Add task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _memcpy - copy memory area
- * @dest: destination 
- * @src: source
- * @n: number
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
