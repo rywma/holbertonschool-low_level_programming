5766586838cbef293237153bde375ac2e0ae47fb
“rywma” <almujallireema@gmail.com>
2026-04-25 18:54:09 +0000

    Add task 0

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string 
+ * @len: length of the string
+ * @next: points to next node
+ *
+ * Description: singly linked lists
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
